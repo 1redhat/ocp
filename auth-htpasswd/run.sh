d3746ef2f5e58b6bcf0fd65dca676c86ec083f1e
Craig Iverson <civerson1@gmail.com>
2020-04-06 11:18:15 -0700

    apply htpasswd.yaml

--- auth-htpasswd/run.sh
+++ auth-htpasswd/run.sh
@@ -1,7 +1,6 @@
 # configure httpasswd in openshift
 oc create secret generic htpass-secret --from-file=htpasswd=users.htpasswd -n openshift-config
-oc create -f htpasswd.yaml
-# oc apply -f htpasswd.yaml
+oc apply -f htpasswd.yaml
 
 # Assign cluster admin to admin users
 #oc adm policy add-role-to-user cluster-admin admin1
